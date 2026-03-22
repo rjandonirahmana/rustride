b85fbaa51040361ed94a5a116dc0d36ddecc0618
rjandonirahmana <rjandonirahmana@gmail.com>
2026-03-22 17:50:50 +0700

    update

--- src/config.rs
+++ src/config.rs
@@ -31,28 +31,3 @@ impl Config {
         })
     }
 }
-
-// impl Config {
-//     pub fn from_env() -> anyhow::Result<Self> {
-//         use std::env;
-
-//         let account_id = env::var("R2_ACCOUNT_ID").expect("R2_ACCOUNT_ID must be set");
-//         let access_key = env::var("R2_ACCESS_KEY_ID").expect("R2_ACCESS_KEY_ID must be set");
-//         let secret_key =
-//             env::var("R2_SECRET_ACCESS_KEY").expect("R2_SECRET_ACCESS_KEY must be set");
-//         let bucket_name = env::var("R2_BUCKET_NAME").unwrap_or_else(|_| "rustride".to_string());
-//         let public_base_url = env::var("R2_PUBLIC_URL").expect("R2_PUBLIC_URL must be set");
-
-//         // Gunakan variabel ini di s3_client config Anda
-
-//         Ok(Self {
-//             database_url: env::var("DATABASE_URL").unwrap_or_else(|_| {
-//                 "mysql://root:LawakKocak1001!@77.237.242.1:3306/ridehailing".into()
-//             }),
-//             redis_url: env::var("REDIS_URL")
-//                 .unwrap_or_else(|_| "redis://:awokawok1001!@77.237.242.1:6379".into()),
-//             jwt_secret: env::var("JWT_SECRET").unwrap_or_else(|_| "default_secret".into()),
-//             grpc_addr: env::var("GRPC_ADDR").unwrap_or_else(|_| "0.0.0.0:50051".into()),
-//         })
-//     }
-// }
